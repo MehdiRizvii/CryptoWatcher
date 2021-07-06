c30ed7ade3b3544dd84a564fdfa46190db61b685
Mehdi Rizvi <mehdirizvi03@gmail.com>
2021-07-06 19:41:35 -0400

    added api, search bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
 import './App.css';
-
+import axios from "axios";
 function App() {
-  return (
+    const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+
+
+    axios.get("  https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=100&page=1&sparkline=false").then(res =>{
+setCoins(res.data);
+    }).catch(error => console.log(error));
+  });
+
+      return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="search">
+  <h1 className="text"> Search for Crypto</h1>
+<form>
+  <input type="text" placeholder="Search" className="coinInput"/>
+
+
+
+</form>
+
+</div>
+
+
+
     </div>
   );
 }
